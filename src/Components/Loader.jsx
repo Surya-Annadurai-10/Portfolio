a705aea77b7ff767605eb2976f5660c054a090c8
Surya Annadurai <suryaannadurai1041999@gmail.com>
2025-03-11 00:42:38 +0530

    Preloader Animation Compeleted

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,108 @@
+import React, { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Loader = () => {
+  let loaderConRef = useRef(null);
+  const fontRef = useRef(null);
+  const overlayRef = useRef(null);
+
+  useGSAP(
+    () => {
+      let ctx = gsap.context(() => {
+        let tl = gsap.timeline();
+        
+
+        tl.from(".box", {
+          yPercent: -100,
+          duration: 2.5,
+          stagger: 0.35,
+          ease: "power3.out",
+        }).to(".box", {
+          yPercent: -100,
+          duration: 2.5,
+          stagger: 0.3,
+          opacity: 0,
+          ease: "power3.out",
+        });
+
+        let wordArr = gsap.utils.toArray(".word");
+       let wordTl = gsap.timeline();
+       wordArr.forEach((ele,index) =>{
+
+        if(index != 2){
+            wordTl.from(ele , {
+                opacity : 0,
+                duration : 1.5,
+              
+                ease: "power3.out"
+            })
+            
+            wordTl.to(ele,{
+             opacity  : 0,
+                duration : 1.5,
+              
+                ease: "power3.out"
+            },"-=0.5")
+        }else{
+            wordTl.from(ele , {
+                opacity : 0,
+                duration : 1.5,
+                
+                ease: "power3.out"
+            })
+            
+            wordTl.to(ele,{
+             opacity  : 0,
+             duration : 1.5,
+             delay : 1,
+                ease: "power3.out"
+            },"-=0.5")
+        }
+       
+       })
+       
+       wordTl.to(".loader" ,{
+       
+           yPercent : -100,
+           duration : 1,
+           ease : "power3.out"
+       },"-=1")
+        
+     
+     
+      });
+
+      return () => ctx.revert();
+    },
+    { scope: [loaderConRef, fontRef , overlayRef] }
+  );
+
+  return (
+    <>
+      <div>
+      <div
+        ref={loaderConRef}
+        className="bg-black w-full loader relative overflow-hidden h-screen flex items-start justify-center"
+      >
+        <div className="box w-[20%] rounded-b-full h-[125%] bg-[rgb(255,0,0,0.95)]"></div>
+        <div className="box w-[20%]  rounded-b-full  h-[125%] bg-[rgba(255,0,0,0.75)]"></div>
+        <div className="box w-[20%]  rounded-b-full h-[125%] bg-[rgba(255,0,0,0.5)]"></div>
+        <div className="box w-[20%]  rounded-b-full h-[125%] bg-[rgb(255,0,0,0.75)]"></div>
+        <div className="box w-[20%]  rounded-b-full h-[125%] bg-[rgb(255,0,0,0.95)]"></div>
+        <div className="w-full h-screen bg-[#ffffff00] absolute  grid place-items-center ">
+          <div ref={fontRef} className="text-white  relative text-center bg-red-600 w-[250px]">
+            <h1 className="font-['Lokicola'] tracking-wider left-[50%] top-[50%] translate-[-50%]  word absolute drop-shadow-2xl  text-6xl">Persistence<sup>!</sup></h1>
+            <h1 className="font-['Lokicola'] tracking-wider left-[50%] top-[50%] translate-[-50%] word  whitespace-nowrap  drop-shadow-2xl  absolute text-6xl">Hardwork<sup>!</sup></h1>
+            <h1 className="font-['Lokicola'] tracking-wider left-[50%] top-[50%] translate-[-50%] text-[#ff0000c0] drop-shadow-2xl  word absolute text-6xl">Consistency<sup>!</sup></h1>
+          </div>
+
+          <div ref={overlayRef} className="w-full overlay top-[100%] h-screen rounded-tl-[50%]  rounded-tr-[50%] bg-[rgba(255,255,255)] absolute"> </div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
